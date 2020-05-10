2caef594ef514d346f8623477542909592bb1320
Ali Zaib <alizaib2706@gmail.com>
2020-05-10 23:12:21 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,283 @@
+import React from 'react';
+import { StyleSheet, Text, View , TouchableOpacity  , Button } from 'react-native';
+import {Questions } from "./Constants/Questions"
+
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props) ;
+   this.state = {
+      myQuestion: Questions , 
+     
+      q: Math.floor(Math.random()*15),
+      score: 0 ,
+      questionNo : 1 ,
+      totalQuestions : 5 ,
+      colorA: "teal" ,
+      colorB: "teal" ,
+      colorC: "teal" ,
+      buttonText: "NEXT" , 
+      
+      screen: "home"
+      
+      
+    
+  } ;
+}
+
+//Next Button
+nextQuestion = () =>{
+  if(this.state.buttonText === "NEXT"){
+  this.setState({q: Math.floor(Math.random()*15) ,
+    colorA: "teal" , colorB: "teal" , colorC: "teal" ,
+    questionNo: this.state.questionNo + 1})
+  if(this.state.questionNo === 4 ){
+    
+      this.setState({buttonText: "Finish"})
+    } 
+  }
+  else if(this.state.buttonText === "Finish"){
+    this.setState({screen: 'home'})
+  }
+}
+
+//sleep function
+sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+//Answer evaluation
+checkAnswer = (ans , option) => {
+  if(this.state.questionNo === 4 ){
+    this.setState({buttonText: "Finish"})
+  }
+  
+  if( this.state.myQuestion[this.state.q].answer === ans ){
+   if(option === "a"){
+   this.setState({colorA: "green"})
+   }
+   else if(option === "b"){
+    this.setState({colorB: "green"})
+   }
+   else if(option === "c"){
+    this.setState({colorC: "green"})
+   }
+   
+   this.setState({score: this.state.score+1})
+   
+   if(this.state.questionNo === 5){
+    this.sleep(600).then(() => this.setState({screen: 'home'} ))
+  }
+
+  
+   this.sleep(600).then(() => this.setState({q: Math.floor(Math.random()*15) ,
+                                             colorA: "teal" , colorB: "teal" ,
+                                            colorC: "teal" , 
+                                            questionNo: this.state.questionNo + 1}))
+  }
+  else{
+    if(this.state.questionNo === 4 ){
+      this.setState({buttonText: "Finish"})
+    }
+    if(option === "a"){
+      this.setState({colorA: "red"})
+      }
+      else if(option === "b"){
+       this.setState({colorB: "red"})
+      }
+      else if(option === "c"){
+       this.setState({colorC: "red"})
+      }
+     
+  if(this.state.questionNo === 5){
+    this.sleep(600).then(() => this.setState({screen: 'home'} ))
+  }
+  this.sleep(600).then(() => this.setState({q:  Math.floor(Math.random()*15)  ,
+    colorA: "teal" , colorB: "teal" ,
+    colorC: "teal" , questionNo: this.state.questionNo + 1}))
+  }
+ }
+  
+  render() {
+const quizScreen = (
+  
+    <View style={styles.container}>
+      <View style={styles.status}>
+        <Text style={{fontSize: 70 , color: "indigo"}}>Score: {this.state.score}</Text>
+        </View>
+      
+      <View style = {styles.Question}>
+      <Text style={{fontSize: 22, color: "lightgrey" , paddingBottom: 20}} >
+        Question {this.state.questionNo}/{this.state.totalQuestions}</Text>
+      
+       <Text style = {{fontSize: 33 , color: "white" , textAlign: "center"  }}> {this.state.myQuestion[this.state.q].question} </Text>
+      </View>
+      
+      
+      <View style = {styles.optionArea}>  
+      <TouchableOpacity 
+       style = {{
+        backgroundColor: this.state.colorA ,
+        alignItems: "center",
+        justifyContent: "center" ,
+        padding: 10 ,
+        margin : 5 , 
+        borderRadius : 5 ,
+        marginBottom : 10 , 
+        width: 400}} 
+    
+      onPress = {() => 
+      this.checkAnswer( this.state.myQuestion[this.state.q].options[0] ,  "a" )}> 
+
+        <Text  style={{color: "white" , fontSize: 28 }}>
+            1.  {this.state.myQuestion[this.state.q].options[0]} </Text>
+        </TouchableOpacity>
+
+      
+        <TouchableOpacity 
+        style = {{ backgroundColor: this.state.colorB ,
+            alignItems: "center",
+            justifyContent: "center" ,
+            padding: 10 ,
+            margin : 5 , 
+            borderRadius : 5 ,
+            marginBottom : 10 , 
+            width: 400}} 
+        
+        onPress = {() => 
+          this.checkAnswer(this.state.myQuestion[this.state.q].options[1] , "b") }>
+        
+        <Text  style={{color: "white" , fontSize: 28 }}>
+            2.  {this.state.myQuestion[this.state.q].options[1]}
+          </Text>
+        </TouchableOpacity>
+
+     
+
+        <TouchableOpacity 
+        style = {{ backgroundColor: this.state.colorC ,
+            alignItems: "center",
+            justifyContent: "center" ,
+            padding: 10 ,
+            margin : 5 , 
+            borderRadius : 5 ,
+            marginBottom : 10 , 
+            width: 400}} 
+        
+        onPress = {() => 
+        this.checkAnswer( this.state.myQuestion[this.state.q].options[2] , "c" )}>
+        <Text  style={{color: "white" , fontSize: 28 }}>
+           3.    {this.state.myQuestion[this.state.q].options[2]}
+          </Text>
+        </TouchableOpacity>
+      
+        </View>
+      
+      
+
+      <TouchableOpacity onPress = {(this.nextQuestion)} activeOpacity= {0.5}>
+       
+       <View style= {styles.nextButton}>
+         <Text style= {{fontSize: 30 , color: "white"}}>{this.state.buttonText}</Text>
+     
+      </View>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const homeScreen  =(
+  
+    <View style = {styles.homescreen} >
+      <View style = {styles.lastScore} >
+      <Text style={{fontSize: 50 , color: "silver"}}>Your Last Score: {this.state.score}</Text>
+      </View>
+     
+    <Text style={{fontSize: 30 , color: "white"}}>General Knowledge Quiz</Text>
+    
+      <TouchableOpacity activeOpacity = {0.8} onPress= { () => this.setState({screen: "quiz" 
+                                                        , score: 0 , questionNo: 1 , buttonText: "NEXT"})} 
+      style={{backgroundColor: "darkslateblue" , padding: 20 , marginTop: 50 ,
+                                borderRadius: 35}}>
+        <Text style={{fontSize: 20 , color: "silver"}}> START QUIZ</Text>
+      </TouchableOpacity>
+   
+     </View>
+    
+  )
+
+  return this.state.screen === 'home' ? homeScreen : quizScreen ;
+}
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rosybrown',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  status: {
+    flexDirection: "row" ,
+    justifyContent: "center",
+    alignItems: "center" ,
+    
+    width: 300 ,
+    padding: 10
+},
+homescreen: {
+  flex: 1,
+  backgroundColor: 'steelblue',
+  alignItems: 'center',
+  justifyContent: 'center',
+  
+},
+lastScore:{
+  paddingBottom: 50
+},
+  Question: {
+    fontSize: 30,
+ 
+    paddingBottom: 30 ,
+    marginTop: 100 ,
+    justifyContent: "center",
+    alignItems: "center",
+    
+    
+  },
+
+
+  options : {
+    flexDirection : "row" ,
+ 
+    alignItems: "center",
+    justifyContent: "center" ,
+    padding: 10 ,
+    margin : 5 , 
+    borderRadius : 5 ,
+    marginBottom : 10 , 
+    width: 400
+  
+  },
+
+  optionArea : {
+    
+    padding: 10 ,
+    paddingBottom: 30
+   
+    
+  },
+  nextButton: {
+    padding: 10 ,
+    backgroundColor: "navy" ,
+    width: 100,
+    justifyContent: "center" ,
+    alignItems: "center",
+    borderRadius:30 ,
+    
+    
+      
+  }
+});
---
+++ Constants/Questions.js
@@ -0,0 +1,102 @@
+
+
+
+
+export const Questions = [
+    //Question 1 
+    { question: "What is the capital of USA ? " ,
+options: ["Washington" , "Newyork" , "London"] ,
+answer: "Washington"} ,
+//Question 2
+{
+    question: "Who is the national poet of Pakistan ?",
+    options: ["Mirza Ghalib" , "Jaun Elia" , "Allama Iqbal"] ,
+    answer: "Allama Iqbal"
+},
+//Question 3
+{
+    question: "What is the capital of Pakistan ? " ,
+    options: ["Islamabad" , "Karachi" , "Lahore"] ,
+    answer: "Islamabad"
+}, 
+//Question 4
+{
+    question: "Who is the current PM of Pakistan" ,
+    options: ["Yousuf Raza Gillani" , "Imran Khan" , "Nawaz Sharif"],
+    answer: "Imran Khan"
+},
+//Question 5
+{
+    question: "What is the national sports of Pakistan",
+    options: ["Cricket" , "Football" , "Hockey"],
+    answer: "Hockey"
+},
+//Question 6
+{
+    question: "Pakistan is located in which continent ? " ,
+    options: ["South Asia" , "East Asia" , "West Asia" , "Central Asia"],
+    answer: "South Asia"
+},
+// 7
+{
+    question: "Which of the following countries is not bordered with Pakistan ?" ,
+    options: [" Afghanistan" , "Bangladesh" , "India"] ,
+    answer: "Bangladesh"
+},
+
+//8
+{
+    question: "Which animal is the national animal of Pakistan?" ,
+    options: ["Lion" , "goat" , "Markhor"] ,
+    answer: "Markhor"
+},
+//9
+{
+    question: "When did Pakistan won the cricket worldcup ?" ,
+    options: ["1992" , "2019" , "1999"],
+    answer: "1992"
+},
+//10
+{
+    question: "Which language is considered the *Lingua-Franca* in Pakistan ? ",
+    options: ["English" , "Pashtu" , "Urdu"],
+    answer: "Urdu" 
+},
+//11
+{
+    question: "Who the CEO of Facebook ?" ,
+    options:[ "Mark Zuckerberg" , "Bill Gates" , "Anil Kapoor" ] ,
+    answer: "Mark Zuckerberg"
+},
+//12
+{
+    question: "Which city in Pakistan is the capital of Punjab ?",
+    options: ["Lahore" , "Rawalpindi" , "Gujrat"],
+    answer: "Lahore"
+},
+//13
+{
+    question: "Which of these persons is a famous singer ? ",
+    options:["Waqar Zaka" , "Babar Azam" , "Ali Sethi"],
+    answer: "Ali Sethi"
+},
+//14
+{
+    question: "Who is the current president of Pakistan ?",
+    options: ["Dr. Arif Alvi" , "Asif Zardari" , "Shehbaz Sharif"],
+    answer: "Dr. Arif Alvi"
+},
+//15
+{
+    question: "Who is the current captain of Pakistan team?",
+    options: ["Sarfaraz" , "Mishbah ul Haq" , "Babar Azam"],
+    answer: "Babar Azam"
+},
+
+
+
+
+
+
+]
+   
